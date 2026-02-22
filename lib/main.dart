dbd102a0f7cdc96cfefbe5b15c5b2375ac9c3180
novincdavis123 <novindavisks@gmail.com>
2026-02-22 21:17:19 +0530

    Initial project setup: Clean Architecture, Riverpod, Firebase, Dio, Hive, Material 3

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(ProviderScope(child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const SmartTask(),
+    );
+  }
+}
+
+class SmartTask extends StatelessWidget {
+  const SmartTask({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
